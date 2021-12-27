662e9db19581f30353295b55a1b566ba4ddd91e2
xhaktmchl <xhaktmchl@naver.com>
2021-12-27 15:22:31 +0900

    android_week05_checkbox code

---
+++ week05_1_EX04_24_CheckBox_Toggle/app/src/main/java/com/example/week05_1_ex04_24_checkbox_toggle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.week05_1_ex04_24_checkbox_toggle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
